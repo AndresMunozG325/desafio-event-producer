42a07680b85fe6b5ba81638d7743f2326d15cc84
Andres <andresmunozgo@gmail.com>
2021-01-25 04:10:46 -0300

    many modifications

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
+    has_many :concerts, dependent: :destroy
     validates :name, presence: true
     validates :debut_date, presence: true
     validates :number_of_members, presence: true
@@ -9,4 +10,9 @@ class Group < ApplicationRecord
     def number_of_concert
         Concert.where(Group_id: id).count
     end
+
+    def total_number_of_people
+        self.concerts.pluck(:number_of_people).sum
+    end
+
 end
