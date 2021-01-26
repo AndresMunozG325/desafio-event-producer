1fc20e089fbb1513a6b50fe437a81d6ec481e89b
Andres <andresmunozgo@gmail.com>
2021-01-26 00:08:43 -0300

    update to show view and group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -15,4 +15,28 @@ class Group < ApplicationRecord
         self.concerts.pluck(:number_of_people).sum
     end
 
+    def concerts_this_month
+        if self.concerts.count > 0
+            self.concerts.where('extract(year from date) = ?', Time.now.year).where('extract(month from date) = ?', Time.now.month).count 
+        else 
+            return 0  
+        end
+    end
+
+    def the_last_concert
+        if self.concerts.count > 0
+            self.concerts.order(date: :asc).last.date
+        else
+            "no debut"
+        end
+    end  
+
+    def amount_of_public
+        self.concerts.pluck(:number_of_people).max
+    end
+
+    def concert_duration
+        self.concerts.pluck(:duration).max
+    end
+
 end
